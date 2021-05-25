e21dca456dc20e6b0fa0cd9892580d33d5e382fd
Biznaz <aldhelm7@gmail.com>
2021-05-25 11:34:30 -0700

    added more code to getUserRepos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,17 @@
-var getUserRepos = function () {
-  fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function (user) {
+  //FORMAT THE GITHUB API URL
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // MAKE A REQUEST TO THE URL
+  fetch(apiUrl).then (function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
 };
 
-getUserRepos();
+
+var response = fetch("https://api.github.com/users/octocat/repos");
+console.log(response);
+
+getUserRepos("mattbisbee");
